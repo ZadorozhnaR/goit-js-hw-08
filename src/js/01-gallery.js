bb0c1e2ad41f6fe5594a246224a1251c24c1da74
Iryna <ira.shkraba@ukr.net>
2023-07-31 22:47:14 +0300

    first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,30 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+
+const addPhotos = galleryItems
+  .map(
+    photo => `<li class="gallery__item">
+  <a class="gallery__link" href="${photo.original}">
+    <img
+      class="gallery__image"
+      src="${photo.preview}"
+      data-source="${photo.original}"
+      alt="${photo.description}"
+    />
+  </a>
+</li>`
+  )
+  .join(' ');
+
+galleryList.insertAdjacentHTML('beforeend', addPhotos);
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
