4dc4bffc767896bf649c244a92514ad10caf77fc
Iryna <ira.shkraba@ukr.net>
2023-08-05 16:04:30 +0300

    second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,35 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const local_key = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    const curTime = data.seconds;
+
+    localStorage.setItem(local_key, JSON.stringify(curTime));
+  }, 1000)
+);
+
+const sec = JSON.parse(localStorage.getItem(local_key));
+
+player
+  .setCurrentTime(sec)
+  .then(function (secondsSave) {
+    secondsSave = sec;
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        secondsSave = 0;
+        break;
+
+      default:
+        secondsSave = 0;
+        break;
+    }
+  });
