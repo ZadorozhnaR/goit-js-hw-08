6be0f2379dc82ad110ac67bcad12d0e482b69d92
Iryna <ira.shkraba@ukr.net>
2023-08-06 17:13:02 +0300

    third task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
+const form = document.querySelector('.feedback-form');
+const localKey = 'feedback-form-state';
+// let formData = JSON.parse(localStorage.getItem(localKey));
 
+let formData = JSON.parse(localStorage.getItem(localKey)) || {};
+
+const { email, message } = form.elements;
+
+updateWindow();
+form.addEventListener('input', throttle(saveText, 500));
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+  console.log({ email: email.value, message: message.value });
+  localStorage.removeItem(localKey);
+  e.currentTarget.reset();
+}
+
+function saveText(e) {
+  formData = {
+    email: email.value,
+    message: message.value,
+  };
+  localStorage.setItem(localKey, JSON.stringify(formData));
+}
+
+function updateWindow() {
+  if (formData) {
+    email.value = formData.email || '';
+    message.value = formData.message || '';
+  }
+}
